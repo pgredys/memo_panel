122d94c02619185c1d2d3634d758a199003e5699
pgredys <gredys.przemyslaw@gmail.com>
2024-05-19 00:19:36 +0200

    better GNU terry pratchett

--- website/__init__.py
+++ website/__init__.py
@@ -3,17 +3,23 @@ from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
 
-
 db = SQLAlchemy()
 DB_NAME = "database.db"
 from .models import User, Note
 from .views import views
 from .auth import auth
 
+
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hjshjhdjah kjshkjdhjs'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+
+    @app.after_request
+    def gnu_terry_pratchett(resp):
+        resp.headers.add('X-Clacks-Overhead', 'GNU Terry Pratchett')
+        return resp
+
     db.init_app(app)
 
     login_manager = LoginManager()
@@ -26,7 +32,6 @@ def create_app():
 
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
-
 
     with app.app_context():
         db.create_all()
